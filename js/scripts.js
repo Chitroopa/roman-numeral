93a3a1c7becb387f1311a8d8ea727ecbd1201d59
Chitroopa Manikkavasagam, Katie Borel and Terry Urich <terry@>
2017-03-30 10:01:18 -0700

    Add spec not to accept number greater than 4000 and lesser than 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
+var romanConverter = function (number) {
+ if((number > 3999) || (number < 1))
+ {
+   alert("Please enter a number between 1 - 4000");
+ }
+ 
 
+}
 
 
 
@@ -7,6 +14,7 @@ $(document).ready(function(){
   $("#form1").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#input").val());
+    var result = romanConverter(userInput);
     console.log(userInput);
   });
 });
