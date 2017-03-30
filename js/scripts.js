b7311539bd63c4fad384e656b69ea03d29fae128
Chitroopa Manikkavasagam, Katie Borel and Terry Urich <chitroopa@>
2017-03-30 10:50:14 -0700

    Add spec for division loop to assign roman letter for each highest divisor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var romanConverter = function (number) {
- var romanNumber = [1,5,10,50,100,500,1000];
- var romanLetter = ["I","V","X","L","C","D","M"];
+ var converted = "";
+ var romanNumber = [1000, 500, 100, 50, 10, 5, 1];
+ var romanLetter = ["M", "D", "C", "L", "X", "V", "I"];
  if((number > 3999) || (number < 1))
  {
    alert("Please enter a number between 1 - 4000");
@@ -8,12 +9,20 @@ var romanConverter = function (number) {
 
  for (var index=0;index<romanNumber.length;index ++)
  {
-   if(romanNumber[index] === number)
+  //  if(romanNumber[index] === number)
+  //  {
+  //    console.log(romanLetter[index]);
+  //  }
+   var quotient = Math.floor(number / romanNumber[index]);
+   var remainder = number % romanNumber[index];
+   for (var j=0; j<quotient && quotient>0 ;j++ )
    {
-     console.log(romanLetter[index]);
+     converted += romanLetter[index];
+    //  console.log(romanLetter[index]);
    }
+   number = remainder;
  }
-
+ console.log(converted);
 }
 
 
@@ -24,5 +33,6 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = parseInt($("#input").val());
     var result = romanConverter(userInput);
+    console.log(result);
   });
 });
