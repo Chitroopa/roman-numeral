cd2503cf990cfd82393e2535620fff07a3312b35
Chitroopa Manikkavasagam, Katie Borel and Terry Urich <katie@>
2017-03-30 10:14:27 -0700

    Add spec to return exact matching roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 var romanConverter = function (number) {
+ var romanNumber = [1,5,10,50,100,500,1000];
+ var romanLetter = ["I","V","X","L","C","D","M"];
  if((number > 3999) || (number < 1))
  {
    alert("Please enter a number between 1 - 4000");
  }
- 
+
+ for (var index=0;index<romanNumber.length;index ++)
+ {
+   if(romanNumber[index] === number)
+   {
+     console.log(romanLetter[index]);
+   }
+ }
 
 }
 
@@ -15,6 +24,5 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = parseInt($("#input").val());
     var result = romanConverter(userInput);
-    console.log(userInput);
   });
 });
